2a80bc5cf21bdf52581c7c11a3d12e4ea1e757aa
Jason Frame <jason@magiclamp.co.uk>
2009-01-22 23:56:02 +0000

    Reformatting

--- javascripts/jquery.autogrow-textarea.js
+++ javascripts/jquery.autogrow-textarea.js
@@ -23,21 +23,21 @@
             }).appendTo(document.body);
             
             var update = function() {
-	
-							  var times = function(string, number) {
-							  	_res = '';
-							  	for(var i = 0; i < number; i ++) {
-							  		_res = _res + string;
-							  	}
-							  	return _res;
-							  };
+    
+                var times = function(string, number) {
+                    var _res = '';
+                    for(var i = 0; i < number; i ++) {
+                        _res = _res + string;
+                    }
+                    return _res;
+                };
                 
                 var val = this.value.replace(/</g, '&lt;')
                                     .replace(/>/g, '&gt;')
                                     .replace(/&/g, '&amp;')
-																		.replace(/\n$/, '<br/>&nbsp;')
+                                    .replace(/\n$/, '<br/>&nbsp;')
                                     .replace(/\n/g, '<br/>')
-																		.replace(/ {2,}/g, function(space) {return times('&nbsp;', space.length -1) + ' '});
+                                    .replace(/ {2,}/g, function(space) { return times('&nbsp;', space.length -1) + ' ' });
                 
                 shadow.html(val);
                 $(this).css('height', Math.max(shadow.height() + 20, minHeight));
